1d4ee223db8cc30cc7b5361d9147fa76b0a519c2
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-09-20 12:29:00 +0500

    scope and let const declaring

---
+++ day3-namaste-JS/temporalDeadZone.js
@@ -0,0 +1,9 @@
+console.log(a);
+console.log(b);
+let a = 10;
+// let a =90;//error of already declared..
+var b=100;
+
+
+// -----------------let const different
+const a =9; //throw error of already declaring before
