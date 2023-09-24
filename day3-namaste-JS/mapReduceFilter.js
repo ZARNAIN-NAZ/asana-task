2142a6400e0b9e062ec3fb49a6c3f4df3c8000c4
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-09-24 19:06:24 +0500

    done with map and its eg

---
+++ day3-namaste-JS/mapReduceFilter.js
@@ -0,0 +1,36 @@
+// //map used to  transform an array
+// const arr =  [3,4,54,34,3]
+//  //we want to double the array
+
+//  function double(x){
+//     return x*2
+//  }
+
+//  //for tripple the array
+//  function tripple(x){
+//     return x*3
+// }
+
+// //for binary ot that array
+// // function binary(x){
+// //     return x.toString(2)
+// // }
+
+
+
+//  const output = arr.map(double)
+//  const output2 = arr.map(tripple)
+// //  const output3= arr.map(binary)
+
+// // 2nd way to do same thing
+// // const output3= arr.map(function binary(x){
+// //     return x.toString(2)
+// // })
+
+// //3rd way to to same thing
+// const arrowFunction=arr.map((x)=>x.toString(2))
+// console.log(arrowFunction);
+
+//  console.log(output);
+//  console.log(output2);
+// // console.log(output3);
