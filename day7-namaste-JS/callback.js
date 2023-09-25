0e4d9435a81b07d69e5b4d29f4736f0a095cc532
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-09-26 01:38:34 +0500

    importance and issues in call back(callback hell , inversion control)

---
+++ day7-namaste-JS/callback.js
@@ -0,0 +1,32 @@
+// console.log("hello");
+// setTimeout(function xyz()  {
+//     console.log("zaini");
+
+// }, 5000);
+// // console.log("i am ");
+// console.log("zarnain");
+
+//e-commerce website example
+//create order
+//payment
+//order summary
+//update wallet
+// const example = ["watches", "shoes", "kurta"];
+// api.createOrder(example, function () {
+//   api.proceedToPayment()
+    // function () {
+//     api.showOrderSummary(function () {
+//       api.updatewallet();
+//     });
+//   });
+// }
+// );
+
+
+
+//eg of inversioin control
+const example = ["watches", "shoes", "kurta"];
+api.createOrder(example, function () {
+  api.proceedToPayment()
+
+});
