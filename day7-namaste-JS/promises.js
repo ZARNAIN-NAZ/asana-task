bff1310a7effe16af725f4b1da64adbfa3943489
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-09-27 18:51:58 +0500

    promise chaining

--- day7-namaste-JS/promises.js
+++ day7-namaste-JS/promises.js
@@ -1,12 +1,36 @@
-//e-commerce example
-const cart = ["shoes" , "pants" , "watches"]
-createOrder(cart , function (orderId) {
-    proceedToPayment(orderId); //this method have issue of inversion control
-});//cart
+// e-commerce example
+const cart = ["shoes", "pants", "watches"];
+createOrder(cart, function (orderId) {
+  proceedToPayment(orderId, function (paymentInfo) {
+    showOrderSummary(paymentInfo, function () {
+      updateWalletBalance();
+    });
+  }); //this method have issue of inversion control
+}); //cart
 
 // then we solve it through promises
-const promise = createOrder(cart);
-promise.then(function (orderId) {
-    proceedToPayment(orderId)
-    
-})
+// const promise = createOrder(cart);
+// promise.then(function (orderId) {
+//     proceedToPayment(orderId , function (paymentInfo){
+//         showOrderSummary(paymentInfo)
+
+//     })
+
+// }),
+createOrder(cart)
+  .then(function (orderId) {
+    proceedToPayment(orderId);
+  })
+  .then(function (orderId) {
+    showOrderSummary(orderId);
+  });
+
+//fetch function
+//promise object in browser
+
+// const gitHub = "https://github.com/ZARNAIN-NAZ"
+// const user =  fetch(gitHub);
+// console.log(user);
+// user.then(function(data){
+//     console.log(data);
+// })
