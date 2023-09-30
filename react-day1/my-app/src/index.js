33b586908ff431e8c300da06da66922b11137721
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-10-01 02:42:15 +0500

    host  1st react app on firebase

--- react-day1/my-app/src/index.js
+++ react-day1/my-app/src/index.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css'
 
-let h =3
-let itemNumber =90
-function Hi(){
-  return <div> hello  <strong>zarnain</strong> 
-  <h1>1st heading</h1>
-  <ul>
-    <li>
-      class
-    </li>
-    <li>
-      subject
-    </li>
-    <li>
-     section
-    </li>
-  </ul>
-  {h+4},{7-0}
-<li>{--itemNumber}</li>
-
-   </div>
-  
+let h = 3;
+let itemNumber = 90;
+function Hi() {
+  return (
+    <div>
+      {" "}
+      hello <strong>zarnain</strong>
+      <h1>1st heading</h1>
+      <ul className="myList">
+        <li>class</li>
+        <li>subject</li>
+        <li>section</li>
+      </ul>
+      {h + 4},{7 - 0}
+      <li>{--itemNumber}</li>
+    </div>
+  );
 }
-ReactDOM.render(<Hi/> ,document.querySelector("#root"))
+ReactDOM.render(<Hi />, document.querySelector("#root"));
