833c5b731ed22246ec98423755079d797a3fdce1
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-10-01 01:11:27 +0500

    basic jsx

--- react-day1/my-app/src/index.js
+++ react-day1/my-app/src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+let h =3
+let itemNumber =90
 function Hi(){
-  return <div> hello world </div>
+  return <div> hello  <strong>zarnain</strong> 
+  <h1>1st heading</h1>
+  <ul>
+    <li>
+      class
+    </li>
+    <li>
+      subject
+    </li>
+    <li>
+     section
+    </li>
+  </ul>
+  {h+4},{7-0}
+<li>{--itemNumber}</li>
+
+   </div>
+  
 }
 ReactDOM.render(<Hi/> ,document.querySelector("#root"))
