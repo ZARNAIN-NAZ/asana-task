b2122d307e82134892fce4f1b7cb2e4cb66c1c19
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-10-03 01:34:23 +0500

    start temperature by using state

--- react-day2/stateful/src/index.js
+++ react-day2/stateful/src/index.js
@@ -4,34 +4,49 @@ import "./index.css";
 const Home = () => {
   // let name = "JOHN";
   const [isLit, setLit] = React.useState(true);
-
+const [temp , setTemp] = React.useState(72)
   // const changeVar = () => {;
   // name = "ali";
   // // setLit("irtiza")
   // console.log("name:" , name);
 
   // }
-  const changeState = () => {
+  // const changeState = () => {
     // setLit(false)
     // setLit(!isLit)
     // console.log("changing state");
-  };
+  // };
   return (
     // <div className={(isLit)?"room lit" :"room dark"}>
     <div className={`room ${isLit ? "lit" : "dark"}`}>
-      <p>light is {isLit ? "on" : "off"} </p>
+
+
+<p> 
+   <button onClick={sub}>-</button>
+   {temp}
+   <button onClick={add}>+</button>
+   </p>
+
+
+
+      {/* <p>light is {isLit ? "on" : "off"} </p> */}
       {/* <button onClick={changeVar}>change variable</button> */}
       {/* <button onClick={changeState}>change state</button> */}
-      <button
+      {/* <button
         onClick={() => {
           setLit(!isLit);
-        }}
-      >
-        Turn{isLit ? "off " : "on"}
-      </button>
+        }} */}
+      {/* > */}
+        {/* Turn{isLit ? "off " : "on"}
+      </button> */}
       {/* inline upperone */}
 
       {/* THIS BUTTON is component & onclick is props */}
+
+<br/>
+<br/>
+
+
     </div>
   );
 };
