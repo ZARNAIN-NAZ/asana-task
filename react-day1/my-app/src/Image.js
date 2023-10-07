30359a15dcfc7696c3cae1cea74a7fe3d1c98c44
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-10-07 21:00:15 +0500

    passing jsx as a prop

--- react-day1/my-app/src/Image.js
+++ react-day1/my-app/src/Image.js
@@ -1,6 +1,7 @@
-function Image (){
+function Image (props){
     return <div>
-         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwBb3IlJxkIZtXG81l9rnjEfKEDa4PhiDBm-IPNsKcCg&s'></img>
+         {/* <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwBb3IlJxkIZtXG81l9rnjEfKEDa4PhiDBm-IPNsKcCg&s'></img> */}
+<img src={props.src} />
 
     </div>
 }
