2fb78d3650411400a40c7668a55b5ec6ae352642
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-10-11 21:09:48 +0500

    build  card

---
+++ tic-tac-toe/src/components/Icon/Icon.jsx
@@ -0,0 +1,14 @@
+import { FaRegCircle, FaTimes } from 'react-icons/fa'
+
+function Icon({name}){
+    if(name == "circle"){
+        return <FaRegCircle/>
+    }
+    else if(name == "cross"){
+        return <FaTimes/>
+    }
+    
+
+
+}
+export default Icon;
