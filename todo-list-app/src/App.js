a1ef013f93a1bf2e0ee594436da3531758d0f7e8
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-10-15 02:13:32 +0500

    complete todoApp

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -1,36 +1,47 @@
-import React, { useState }  from "react";
-import {} from "./App.css"
+import React, { useState } from "react";
+import {} from "./App.css";
+import ToDoLists from "./ToDoLists";
 
-function App(){
-  const  [inputList , setInputList] = useState("buy apple")
-const [items , setItems]   = useState([])
+function App() {
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
 
-  function itemEvent(event){
-    setInputList(event.target.value)
-  
+  function itemEvent(event) {
+    setInputList(event.target.value);
   }
-  function listOfItem(){
-
+  function listOfItem() {
+    setItems((oldItems)=>{
+      return [...oldItems , inputList]
+    })
+    setInputList("")
   }
-return (
 
-  <>
-  <div className="main-div">
-    <div className="center-div">
-      <br/>
-      <h1 className="top-heading">toDo List App</h1>
-      <br/>
-    
-<input type="text" placeholder="Add an item" onChange={itemEvent}/>
-<button onClick={listOfItem}>+</button>
-<ol>
-  <li>{inputList}</li>
-</ol>
-    </div>
+  const deleteItems = (()=>{
+    console.log("deleted");
+})
+  return (
+    <>
+      <div className="main-div">
+        <div className="center-div">
+          <br />
+          <h1 className="top-heading">toDo List App</h1>
+          <br />
 
-
-  </div>
-  </>
-)
+          <input type="text" placeholder="Add an item" 
+          value={inputList}
+          onChange={itemEvent} />
+          <button onClick={listOfItem}>+</button>
+          <ol>
+            {Items.map((cevent , index) => {
+                  return <ToDoLists key={index}
+                  id={index}
+                  text={cevent}   
+                  onselect={deleteItems} />
+})}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
 }
 export default App;
