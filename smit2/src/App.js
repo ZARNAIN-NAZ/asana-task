99fd4fbac0a4652a510a15fa8676ab45a3bd1c1a
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-10-15 11:06:59 +0500

    used material ui

---
+++ smit2/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import MyNavbar from './components/Navbar';
+import MyCard from './components/Card';
+
+function App() {
+  return (
+   <>
+    
+    <MyNavbar />
+    <MyCard />
+    </>
+  );
+}
+
+export default App;
+//search mui discover core lib  and copy pastelink of material ui from browser.
