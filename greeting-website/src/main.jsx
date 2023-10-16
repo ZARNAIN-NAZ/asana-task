632f22827836fadea00c64e86b417c7feea323a7
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-10-16 16:19:19 +0500

    react greeting website according to time

---
+++ greeting-website/src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+let currDate  = new Date(2023 ,11,23,19)
+currDate = currDate.getHours();
+let greeting = ""
+const  cssStyle= {};
+
+if(currDate >=1 && currDate <=12){
+ greeting ="Good Morning"
+ cssStyle.color="Green";
+}
+else if(currDate >=12 && currDate <=17){
+  greeting = "Good Evening"
+  cssStyle.color="Orange";
+}
+else{
+  greeting ="Good night"
+  cssStyle.color="blue";
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+<>
+<div className='main-div'>
+<h1> Hello Sir , <span style={cssStyle}>{greeting}</span>  </h1>
+
+</div>
+
+</>  )
+
