b1ff14ad3c681c02d1b1788a94ccac59d08d74ee
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-10-18 16:09:52 +0500

    5 cards done

--- netflix-app/src/App.js
+++ netflix-app/src/App.js
@@ -1,29 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <>
-<div className='cards'>
-  <div className='card'>
-  <img src='https://wallpapercave.com/wp/wp4056410.jpg'
-  alt='myPic' className='card__img'/>
-  <div className='card__info'>
-    <span className='card___Category'>
-A Netflix Orignal Series
-    </span>
-<h3 className='card__title'>DARK</h3>
-<a href='https://www.netflix.com/in/title/80990668?source=35' target='_blank'>
-  <button>Watch Now</button>
-</a>
-  </div>
-
-
-  </div>
-</div>
-
-    </>
-  );
+function App(props) {
+  console.log(props);
+  return 
 }
 
 export default App;
