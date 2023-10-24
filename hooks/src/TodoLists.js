eb25d20ec0ee4840b75fc7d94dd706f7ac6c692a
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-10-24 17:00:30 +0500

    counter

---
+++ hooks/src/TodoLists.js
@@ -0,0 +1,24 @@
+import { React , useState} from 'react';
+
+function TodoLists (){
+    const [ ] = useState("");
+   return(
+    <>
+    <div className='main_div'>
+      <div className='center_div'>
+
+        <h1>
+          0
+        </h1>
+        <div className='btn_div'>
+        <button >Increm</button>
+        <button>Decrem</button>
+        </div>
+
+      </div>
+
+    </div>
+    </>
+   )
+}
+export default TodoLists
