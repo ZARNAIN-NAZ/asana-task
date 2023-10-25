8b816b908f9f6acc88190c3f266e1df2bd1b06f3
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-10-25 16:31:02 +0500

    try material ui

--- hooks/src/TodoLists.js
+++ hooks/src/TodoLists.js
@@ -1,42 +1,33 @@
-import React , {  useState} from 'react';
+import React, { useState } from "react";
+// import AddIcon from '@mui/icons-material/Add';
+function TodoLists() {
+  const [num, setNum] = useState(0);
 
-function TodoLists (){
-    const [ num , setNum] = useState(0);
-
-    const incNum = ()=>{
-      setNum(num+1)
-  
+  const incNum = () => {
+    setNum(num + 1);
+  };
+  const decNum = () => {
+    if (num > 0) {
+      setNum(num - 1);
+    } else {
+      setNum(0);
+      alert("you reached a limit!");
     }
-    const decNum =()=>{
-      if (num>0) {
-
-        setNum(num-1)
-      }
-      else{
-        setNum(0)
-        alert('you reached a limit!')
-      }
-    }
-   return(
+  };
+  return (
     <>
-    <div className='main_div'>
-      <div className='center_div'>
-
-        <div className='heading'>
-     <h1>
-        {num}
-        </h1>
-      
+      <div className="main_div">
+        <div className="center_div">
+          <div className="heading">
+            <h1>{num}</h1>
+          </div>
+          <div className="btn_div">
+            <button onClick={incNum}>Increm</button>
+            <button onClick={decNum}>Decrem</button>
+          </div>
         </div>
-        <div className='btn_div'>
-        <button onClick={incNum}>Increm</button>
-        <button onClick={decNum}>Decrem</button>
-        </div>
-       
-      </div> 
-
-    </div>
+      </div>
     </>
-   )
+  );
 }
-export default TodoLists
+export default TodoLists;
